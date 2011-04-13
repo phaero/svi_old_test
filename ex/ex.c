1f88d7895f172e0f06f4080195a78e6fafb59420
Johan Segolsson <johan.segolsson@gmail.com>
2011-04-13 21:21:05 +0200

    Added a second alternativ.

--- ex/ex.c
+++ ex/ex.c
@@ -30,3 +30,36 @@ s_test_group(
 s_test_groups(
 		buffer_tests
 	) // no semicolon
+
+// Alternative 2
+// 
+void test_func( void* handle ) {
+	// Do Tests
+}
+
+void test_setup( void* handle ) {
+	void* data = malloc();
+	s_test_set_data( handle, data );
+}
+void test_func_f( void* handle ) {
+	void* data = s_test_get_data( handle );
+	// Do Tests
+}
+void test_teardown( void* handle ) {
+	void* data = s_test_get_data( handle );
+	free( data );
+}
+
+void test_group( void* handle ) {
+	s_test_add_test( handle, "Test name", test_func );
+	s_test_add_test_f( handle, "Test name", test_func_f, test_setup, test_teardown );
+
+	return;
+}
+
+int main( int argc, const char* argv[] ) {
+	s_test_init( handle );
+	s_test_add_group( handle, "Group name", test_group_fp );
+
+	return s_test_main( argc, argv, handle );
+}
